0ed63a724cb9abdd24ffd763d6f84628e1e0f717
Fhad Abdulelah Almusharraf <fahad.almshrf@gmail.com>
2026-08-07 14:08:08 +0300

    Set default API base URL to /api

--- lib/api-client-react/src/custom-fetch.ts
+++ lib/api-client-react/src/custom-fetch.ts
@@ -15,7 +15,7 @@ const DEFAULT_JSON_ACCEPT = "application/json, application/problem+json";
 // Module-level configuration
 // ---------------------------------------------------------------------------
 
-let _baseUrl: string | null = null;
+let _baseUrl: string | null = "/api";
 let _authTokenGetter: AuthTokenGetter | null = null;
 
 /**
